8115d4d830ac6ef4c4f1d8f8c21d2ebc608a12e1
Smiatana <polarmonkey000@gmail.com>
2025-11-21 11:40:13 +0300

    crud for products and categories

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private readonly ShopContext _context;
+
+    public CategoriesController(ShopContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Category>> >GetCategories()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+
+    [HttpGet("id")]
+    public async Task<ActionResult<Category>> GetCategory(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return category;
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    public async Task<ActionResult<Category>> CreateCategory(Category category)
+    {
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetCategory), new { id = category.Id }, category);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateCategory(int id, Category category)
+    {
+        if (id != category.Id)
+            return BadRequest();
+
+        _context.Entry(category).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.Categories.Any(e => e.Id == id))
+                return NotFound();
+            else
+                throw;
+        }
+
+        return NoContent();
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCategory(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+            return NotFound();
+
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
